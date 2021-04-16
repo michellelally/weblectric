9bd0289a7e7964a0f7d2b55bb8d0627aceb298bd
Shelley <shelley.lally@gmail.com>
2021-04-16 22:17:27 +0100

    some comments done, need to work on css

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,4 +1,7 @@
-var loggedIn =false;
+var errorMsg = document.getElementById("error");
+
+var loggedIn = false;
+
 window.onload = function () {
 
     // using an eventListener to register when the user clicks the submit button
@@ -7,32 +10,36 @@ window.onload = function () {
         event.preventDefault();
 
         // getting the users email address from the form
-        var email = document.getElementById("email").value;
+        //var email = document.getElementById("email").value;
 
         // getting the users name from the form
-        var name = document.getElementById("firstname").value;
+        //var name = document.getElementById("firstname").value;
 
         // this is an object that is used by the EmailJS template for the email
         // these are needed to have the users name displayed in the email and to send it to their inbox
         // setting the variables to the users email and name
+        var email = document.getElementById("email").value;
+        var firstname = document.getElementById("firstname").value;
+
+
         var templateParams = {
             email: email,
-            name: name,
+            name: firstname
         };
 
         // this function is responsible for sending the email 
         // the paramters are unique identifiers provided on our EmailJS account
         // the first parameter is a service id, which is what connects our email address with EmailJS 
         // the second is the template id, this will determine what the email says and is set up on the EmailJS dashboard
-        if (loggedIn == true){
+        if (loggedIn == true) {
             emailjs.send('service_he5dnbc', 'template_9hbccd7', templateParams)
-            .then(function () {
-                // displays success to the console if the email was sent
-                console.log('SUCCESS!');
-            }, function (error) {
-                // displays failed to the console if the email was not sent
-                console.log('FAILED...', error);
-            });
+                .then(function () {
+                    // displays success to the console if the email was sent
+                    console.log('SUCCESS!');
+                }, function (error) {
+                    // displays failed to the console if the email was not sent
+                    console.log('FAILED...', error);
+                });
         }
 
     });
@@ -53,44 +60,44 @@ function validateForm() {
 }
 
 function validateEmail(email) {
-    alert("validateEmail()");
-
+    // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    // using a code snippet from the stack overflow link above, I used regular expression matching 
+    // to check that the email address is valid
+    // this expression will pass anything that matches this syntax:
+    // anystring@anystring.anystring
     var re = /\S+@\S+\.\S+/;
     if (re.test(email)) {
-        console.log("correct email");
+        // once the email is validated, call the store() function 
         store();
+    } else {
+        // if email is invalid, display this error message 
+        errorMsg.innerHTML += "Error: Invalid email address <br>";
     }
 }
 
 function store() {
-
-    var firstname = document.getElementById("firstname");
-    var surname = document.getElementById("surname");
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-
-    localStorage.firstname = firstname.value;
-    localStorage.surname = surname.value;
-    localStorage.password = password.value;
-    localStorage.email = email.value;
+    var firstname = document.getElementById("firstname").value;
+    var surname = document.getElementById("surname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    localStorage.firstname = firstname;
+    localStorage.surname = surname;
+    localStorage.password = password;
+    localStorage.email = email;
     localStorage.loggedIn = true;
     loggedIn = true;
-    //  window.open("index.html");
+    // window.open("index.html");
 }
 
 function signIn() {
-
-
-
     //calling the validateForm() function
     validateForm();
 
-    // getting the length of the value of the element with an id of password from the HTML 
     var password = document.getElementById("password").value;
     var passwordCheck = document.getElementById("password-check").value;
+    var email = document.getElementById("email").value;
 
     // checking if the length is >=8
-
     if (password.length >= 8) {
 
         // found this method on w3schools.com when researching the best way to compare strings 
@@ -99,16 +106,15 @@ function signIn() {
         // locale compare returns 0 or 1, 0 if the strings are the same or 1 if they are incorrect
         // checking if x = 0
         if (x == 0) {
+            validateEmail(email);
             store();
         } else {
-            document.getElementById("error").innerHTML = "Error:";
-            document.getElementById("match-error").innerHTML = "Passwords must match.";
+            errorMsg.innerHTML += "Error: Passwords must match. <br>";
         }
 
     } else {
         // update the html of the elements with id of error and login_error
-        document.getElementById("error").innerHTML = "Error:"
-        document.getElementById("length-error").innerHTML = "Password must be 8 characters in length"
+        errorMsg.innerHTML += "Error: Password must be 8 characters in length. <br>"
     }
 }
 
--- js/tickets.js
+++ js/tickets.js
@@ -28,7 +28,6 @@ function ready() {
 
 function purchaseClicked() {
     // check if logged in 
-    alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
