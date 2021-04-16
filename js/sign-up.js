80c70c20b34938c4f7510ebee98f74bf0d696321
Shelley <shelley.lally@gmail.com>
2021-04-16 19:39:32 +0100

    EmailJS ready to implement in signup

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,6 +1,5 @@
-
 function store() {
-    console.log("store()")
+    console.log("store()");
 
     var firstname = document.getElementById("firstname");
     var surname = document.getElementById("surname");
@@ -18,59 +17,66 @@ function store() {
 }
 
 
-function validatePassword() {
+function checkPasswordLength() {
 
     var password = document.getElementById("password").value;
-
     var passwordCheck = document.getElementById("password-check").value;
 
 
     if (password.length < 8) {
+        alert("passwordlength if");
+
         // update the html of the elements with id of error and login_error
-        document.getElementById("error").innerHTML = "Error:"
-        document.getElementById("length-error").innerHTML = "Password must be 8 characters in length"
+        document.getElementById("error").innerHTML = "Error:";
+        document.getElementById("length-error").innerHTML = "Password must be 8 characters in length";
+        return false;
+
+    } else {
+        alert("passwordlength else");
+        confirmPasswords();
+    }
+
+}
+
+function confirmPasswords() {
+    alert("confirmPasswords");
+    var password = document.getElementById("password").value;
+
+    var passwordCheck = document.getElementById("password-check").value;
+
+    // checking if the passwords match
+    if (passwordCheck != password) {
+        // alert the user
+        alert("confirmPasswords if");
+
+        // update the html of the elements with id of error and login_error
+        document.getElementById("error").innerHTML = "Error:";
+        document.getElementById("match-error").innerHTML = "Passwords must match.";
         return false;
     } else {
         // removing the error messages
-        document.getElementById("error").innerHTML = ""
-        document.getElementById("login_error").innerHTML = ""
+        alert("confirmPasswords else");
+        store();
+
+        // document.getElementById("error").innerHTML = ""
+        // document.getElementById("match-error").innerHTML = ""
     }
-
-    // //checking if the password is left blank 
-    // if (password == null || password == "") {
-
-    //     console.log(password)
-
-    //     // alert the user
-    //     alert("Password can not be blank.");
-    //     return false; 
-
-    // } 
-
-
-    // // checking if the passwords match
-    // if (passwordCheck != password) {
-    //     // alert the user
-    //     alert("Password match");
-
-    //     // update the html of the elements with id of error and login_error
-    //     document.getElementById("error").innerHTML = "Error:"
-    //     document.getElementById("match-error").innerHTML = "Passwords must match."
-    //     return false;
-    // }
 
 }
 
 function validateEmail(email) {
 
     var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+    if (re.test(email)) {
+        console.log("correct email");
+    }
+
+
 }
 
 function signIn() {
 
     //calling the validateForm() function
-    validatePassword();
-    //  validateEmail();
+    checkPasswordLength();    //  validateEmail();
     // store();
 }
